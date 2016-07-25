Mi-AIoT/linaro-abe

Use ${host} inside manifest, rather than explicit host type.
If a manifest created by a build on an x86_64 host is used on an
aarch64 host, then the build fails, because the *_configure
parameters include the fixed string x86_64-unknown-linux-gnu.
Therefore, when outputting the manifest, the host is rewritten
to ${host}.

Change-Id: I6748bfc2971f264c8cbf7c32a14ecab0bbadbe24

lib/package.sh
@@ -252,7 +252,7 @@ manifest()
 	    echo "${component}_branch=branch=`get_component_branch ${component}`" >> ${tmpfile}
 	    echo "${component}_revision=`get_component_revision ${component}`" >> ${tmpfile}
 	    echo "${component}_filespec=`get_component_filespec ${component}`" >> ${tmpfile}
-	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g"`"
+	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    echo "${component}_configure=\"${configure}\"" >> ${tmpfile}
 	    echo "" >> ${tmpfile}
 	    continue
@@ -286,7 +286,7 @@ manifest()
 	    echo "${component}_filespec=${filespec}" >> ${outfile}
 	fi
 
-	local makeflags="`get_component_makeflags ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g"`"
+	local makeflags="`get_component_makeflags ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	if test x"${makeflags}" != x; then
 	    echo "${component}_makeflags=\"${makeflags}\"" >> ${outfile}
 	fi
@@ -295,7 +295,7 @@ manifest()
 	if test x"${component}" = x"gcc"; then
 	    echo "${component}_configure=" >> ${outfile}
 	else
-	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g"`"
+	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    if test x"${configure}" != x; then
 		echo "${component}_configure=\"${configure}\"" >> ${outfile}
 	    fi
@@ -307,9 +307,9 @@ manifest()
 	fi
 
 	if test x"${component}" = x"gcc"; then
-	    local stage1="`get_component_configure gcc stage1 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g"`"
+	    local stage1="`get_component_configure gcc stage1 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    echo "gcc_stage1_flags=\"${stage1}\"" >> ${outfile}
-	    local stage2="`get_component_configure gcc stage2 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g"`"
+	    local stage2="`get_component_configure gcc stage2 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${snapshots}:\$\{snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    echo "gcc_stage2_flags=\"${stage2}\"" >> ${outfile}
 	fi
 

gtalusan/abe

lib/package.sh
@@ -257,7 +257,7 @@ manifest()
 	    echo "${component}_branch=branch=`get_component_branch ${component}`" >> ${tmpfile}
 	    echo "${component}_revision=`get_component_revision ${component}`" >> ${tmpfile}
 	    echo "${component}_filespec=`get_component_filespec ${component}`" >> ${tmpfile}
-	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g"`"
+	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    echo "${component}_configure=\"${configure}\"" >> ${tmpfile}
 	    echo "" >> ${tmpfile}
 	    continue
@@ -291,7 +291,7 @@ manifest()
 	    echo "${component}_filespec=${filespec}" >> ${outfile}
 	fi
 
-	local makeflags="`get_component_makeflags ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g"`"
+	local makeflags="`get_component_makeflags ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	if test x"${makeflags}" != x; then
 	    echo "${component}_makeflags=\"${makeflags}\"" >> ${outfile}
 	fi
@@ -300,7 +300,7 @@ manifest()
 	if test x"${component}" = x"gcc"; then
 	    echo "${component}_configure=" >> ${outfile}
 	else
-	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g"`"
+	    local configure="`get_component_configure ${component} | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    if test x"${configure}" != x; then
 		echo "${component}_configure=\"${configure}\"" >> ${outfile}
 	    fi
@@ -312,9 +312,9 @@ manifest()
 	fi
 
 	if test x"${component}" = x"gcc"; then
-	    local stage1="`get_component_configure gcc stage1 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g"`"
+	    local stage1="`get_component_configure gcc stage1 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    echo "gcc_stage1_flags=\"${stage1}\"" >> ${outfile}
-	    local stage2="`get_component_configure gcc stage2 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g"`"
+	    local stage2="`get_component_configure gcc stage2 | sed -e "s:${local_builds}:\$\{local_builds\}:g" -e "s:${sysroots}:\$\{sysroots\}:g" -e "s:${local_snapshots}:\$\{local_snapshots\}:g" -e "s:${host}:\$\{host\}:g"`"
 	    echo "gcc_stage2_flags=\"${stage2}\"" >> ${outfile}
 	fi
 