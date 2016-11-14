cd3ba7f4e12c75836b70ca74fc92dc8ffa61b133
Christophe Lyon <christophe.lyon@linaro.org>
2016-11-14 22:51:07 +0100

    jenkins.sh: Make wget less verbose.
    When downloading qemu.
    
    Change-Id: I840697b23eb58e7fa280fbca2fc569694eb6a5fa

--- jenkins.sh
+++ jenkins.sh
@@ -290,7 +290,7 @@ fi
 # Download QEMU provided by Peter Maydell.
 # The tarball has README with version information.
 if [ x"$(uname -m)" = x"x86_64" ]; then
-    wget http://people.linaro.org/~maxim.kuvyrkov/qemu-20160707.tgz
+    wget --progress=dot:giga http://people.linaro.org/~maxim.kuvyrkov/qemu-20160707.tgz
     tar xf qemu-20160707.tgz
     export PATH="$(pwd)/qemu-wip:$PATH"
     for i in aarch64 arm armeb; do
