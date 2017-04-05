9c8f419e5fe015df3994a4321ef11b5cdde1131d
Charles Baylis <charles.baylis@linaro.org>
2017-04-05 12:59:32 +0100

    Use make -j ${cpus} for make check on armeb targets.
    All armeb testing is done with qemu on the build host, so using
    make -j $((2*cpus)) results in excessive system load.
    
    Change-Id: Iad4beb2c9883bd42c249bd0fb0547ad6559e43d2

--- lib/make.sh
+++ lib/make.sh
@@ -622,7 +622,7 @@ make_check()
     if test x"${parallel}" = x"yes"; then
 	local make_flags
 	case "${target}" in
-	    "$build"|*"-elf"*) make_flags="${make_flags} -j ${cpus}" ;;
+	    "$build"|*"-elf"*|armeb*) make_flags="${make_flags} -j ${cpus}" ;;
 	    # Double parallelization when running tests on remote boards
 	    # to avoid host idling when waiting for the board.
 	    *) make_flags="${make_flags} -j $((2*${cpus}))" ;;
